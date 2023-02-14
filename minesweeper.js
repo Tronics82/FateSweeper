19e3d4b8c07b0e74d6e20ae197faa5c07992a00b
Leon <leon.chow1@ontariotechu.net>
2023-02-14 18:30:29 -0500

    Init commit, initialize all files and add skeleton functions

---
+++ minesweeper.js
@@ -0,0 +1,44 @@
+// starts the game, calls create board and call time
+function start() {}
+
+// creates the board
+function constructBoard() {}
+
+// place mines, happens after first reveal
+function placeMines() {}
+
+// chooses game difficulity
+function selectMode() {}
+
+// pauses game
+function pause() {}
+
+// restart the game if game is over
+function restart() {}
+
+// checks if user wins or loses
+function gameOver() {}
+
+// reveals the tile that the user clicked
+function clickTile() {}
+
+// reveals adjacent tiles and numbers depending on user click
+function checkAdjacent() {}
+
+// helper function to check adjacent tiles
+function checkAdjacentHelper() {}
+
+// marks the cleared tiles with numbers if there is adjacent mines
+function placeNumber() {}
+
+// place a flag on a tile
+function flag() {}
+
+// starts timer
+function startTime() {}
+
+// stops timer
+function stopTime() {}
+
+// reveals tiles
+function revealTile() {}

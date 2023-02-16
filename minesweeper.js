8283f71a1f6a4a52f48ea4f809f5724ea1495b6d
Leon <leon.chow1@ontariotechu.net>
2023-02-16 18:47:10 -0500

    replace start onclick method, add cellsData, add more props to grid tiles

--- minesweeper.js
+++ minesweeper.js
@@ -1,19 +1,27 @@
+let cellsData = [];
+
 // starts the game, calls create board and call time
 function start() {
-    constructBoard();
+  constructBoard();
 }
 
 // creates the board
 function constructBoard() {
-    document.getElementById("titleScreen").style.display = "none";
-    const board = document.getElementsByClassName("board")[0];
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            const grid = `<span class="cell"></span>`;
-            board.innerHTML += grid;
-        }
-        //board.innerHTML += `<br />`;
+  document.getElementById("titleScreen").style.display = "none";
+  const board = document.getElementsByClassName("board")[0];
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const id = i.toString() + j.toString();
+      const grid = `<span onclick="clickTile(${id})" id="cell-${id}" class="cell"></span>`;
+      board.innerHTML += grid;
+      cellsData.push({
+        id: i.toString() + j.toString(),
+        revealed: false,
+        hasMine: false,
+      });
     }
+  }
+  console.log(cellsData);
 }
 
 // place mines, happens after first reveal
@@ -32,7 +40,14 @@ function restart() {}
 function gameOver() {}
 
 // reveals the tile that the user clicked
-function clickTile() {}
+function clickTile(id) {
+  // click a tile by adding onclick to the cell, capture click event and use the event to determine if mouse was right or left clicked
+  // replace or mark the cell as clicked
+  // then mark the cell programmatically as hasBeenClicked
+  // if it's first clicked tile, place mines
+  // check adjacent and place numbers
+  console.log(id);
+}
 
 // reveals adjacent tiles and numbers depending on user click
 function checkAdjacent() {}

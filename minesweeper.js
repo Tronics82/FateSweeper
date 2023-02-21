224edfcbc0bee613a83aa9c444fe65e7a3d2adc3
Leon <leon.chow1@ontariotechu.net>
2023-02-21 00:07:03 -0500

    Add cell flagging logic and flag styling, add base game over logic

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,6 @@
 let cellsData = [];
-let firstClick = true;
+let isFirstClick = true;
+let isGameOver = false;
 
 // starts the game, calls create board and call time
 function start() {
@@ -13,14 +14,20 @@ function constructBoard() {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const id = i.toString() + j.toString();
-      const grid = `<span onclick="clickTile(${Number(
-        id
-      )})" id="cell-${id}" class="cell"></span>`;
+      const grid = `
+        <span
+          onmousedown="clickTile(event, ${Number(id)})" 
+          id="cell-${id}" 
+          oncontextmenu="return false;"
+          class="cell">
+        </span>
+      `;
       board.innerHTML += grid;
       cellsData.push({
         id: i.toString() + j.toString(),
         revealed: false,
         hasMine: false,
+        hasFlag: false,
       });
     }
   }
@@ -54,28 +61,42 @@ function pause() {}
 function restart() {}
 
 // checks if user wins or loses
-function gameOver() {}
+function gameOver() {
+  const board = document.getElementsByClassName("board")[0];
+  board.innerHTML = "";
+  alert("Game is over, now what?");
+}
 
 // reveals the tile that the user clicked
-function clickTile(id) {
+function clickTile(mouseEvent, id) {
   // click a tile by adding onclick to the cell, capture click event and use the event to determine if mouse was right or left clicked
   // replace or mark the cell as clicked
   // then mark the cell programmatically as hasBeenClicked
   // if it's first clicked tile, place mines
   // check adjacent and place numbers
-  if (cellsData[id].hasMine) {
-    gameOver();
-    return;
-  }
   const cell = document.getElementById(`cell-${id}`);
-  cell.classList.add("cell-clicked");
-  cellsData[id].revealed = true;
-  if (firstClick) {
-    placeMines();
-    firstClick = false;
+  if (mouseEvent.button === 0 && !cellsData[id].hasFlag) {
+    if (cellsData[id].hasMine) {
+      isGameOver = true;
+      gameOver();
+      return;
+    }
+    cell.classList.add("cell-clicked");
+    cellsData[id].revealed = true;
+    if (isFirstClick) {
+      placeMines();
+      isFirstClick = false;
+    }
+  } else if (mouseEvent.button === 2) {
+    console.log(cellsData[id].hasFlag);
+    if (cellsData[id].hasFlag) {
+      cellsData[id].hasFlag = false;
+      cell.classList.remove("flagged");
+    } else {
+      cellsData[id].hasFlag = true;
+      cell.classList.add("flagged");
+    }
   }
-  //console.log(cellsData);
-  //console.log(cell);
 }
 
 // reveals adjacent tiles and numbers depending on user click
